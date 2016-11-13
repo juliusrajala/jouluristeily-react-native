f28d305907ab629fea9c49949fb30f70d6111ca0
Julius Rajala <jijraj@utu.fi>
2016-11-13 22:52:10 +0200

    Built basic ScheduleView.

--- src/components/ScheduleView.js
+++ src/components/ScheduleView.js
@@ -1,1 +1,29 @@
 import React, { PropTypes } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+
+const ScheduleView = ({ schedules }) => {
+  return (
+    <View style={ styles.schedules } >
+      <Text style={ styles.schedulesTitle }>Aikataulu</Text>
+    </View>
+  );
+} 
+
+ScheduleView.propTypes = {
+  schedules: PropTypes.object
+};
+
+const styles = StyleSheet.create({
+  schedules: {
+
+  },
+  schedulesTitle: {
+
+  }
+});
+
+export default connect(
+  (state, props) => ({
+    schedules: state.schedules
+  }))(ScheduleView);
--- src/core/AppRouter.js
+++ src/core/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LandingView from '../components/LandingView';
 import CabinView from '../components/CabinView';
+import ScheduleView from '../components/ScheduleView';
 
 export default function AppRouter(view, props){
   switch(view){
@@ -8,6 +9,8 @@ export default function AppRouter(view, props){
       console.log('Moving to cabin view');
       return <CabinView props />
     case 'SCHEDULE':
+      console.log('Moving to SCHEDULE');
+      return <ScheduleView props />
     case 'MAP':
     default:
       return <CabinView props />;
