97d81d4e79bd7cb36bcd23f55f2a4c59f0462a7c
Samuli Suomi <sasuomi@gmail.com>
2016-11-21 23:29:58 +0200

    Implement map store and image switching without keeping the scale

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -1,33 +1,51 @@
 import React, { PropTypes } from 'react';
-import { View, ScrollView, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import { mapActions } from '../stores/Maps';
 import PhotoView from 'react-native-photo-view';
 
-const MapView = ({ categories }) => {
-  console.log('map', categories.toJS())
-  const readyCategories = categories.get('categories') || false;
+const MapView = ({ maps, switchMap }) => {
+  const readyMaps = maps.toJS();
+  const mapTags = readyMaps.mapTags;
+  const selected = readyMaps.selected;
+  const defaultFile = readyMaps.defaultFile;
+  const images = {
+      default: require('./images/map.png'),
+      shops: require('./images/map_shops.png'),
+      bars: require('./images/map_bars.png'),
+      restaurants: require('./images/map_restaurants.png')
+  }
 
   return (
-    <View style={ styles.map } >
+    <View style={ styles.container }>
       <PhotoView
-        style={styles.photoView }
-        source={require('./images/map.png')}
+        style={ styles.map }
+        source={ selected ? images[selected] : images.default }
         minimumZoomScale={1}
         maximumZoomScale={4}
+        fadeDuration={0}
         androidScaleType="centerInside"
-        onLoad={() => console.log("Image loaded!")}
         style={{flex: 1}} />
-      <View style={ styles.categories }>
-          <Text style={ styles.category }>Ostokset</Text>
-          <Text style={ styles.category }>Baarit</Text>
-          <Text style={ styles.category }>Ravintolat</Text>
+      <View style={ styles.tags }>
+        <TouchableOpacity
+          onPress={() => switchMap(mapTags.shops)}>
+          <Text style={ [styles.tag, selected === mapTags.shops && styles.activeTag] }>Ostokset</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => switchMap(mapTags.bars)}>
+          <Text style={ [styles.tag, selected === mapTags.bars && styles.activeTag] }>Baarit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => switchMap(mapTags.restaurants)}>
+          <Text style={ [styles.tag, selected === mapTags.restaurants && styles.activeTag] }>Ravintolat</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  map: {
+  container: {
     backgroundColor: '#2f2f2f',
     flex: 1,
     bottom: 0,
@@ -35,26 +53,37 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0
   },
-  photoView: {
+  map: {
     backgroundColor: '#2f2f2f'
   },
-  categories: {
-    height: 50,
+  tags: {
+    height: 72,
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-  category: {
+  tag: {
     fontWeight: '400',
     fontSize: 20,
     color: 'gainsboro',
   },
+  activeTag: {
+    color: '#ffea00'
+  }
 });
 
 MapView.propTypes = {
-  categories: PropTypes.object
+  maps: PropTypes.object,
+  switchMap: PropTypes.func
 };
 
 export default connect(
   (state, props) => ({
-    categories: state.categories
-  }))(MapView);
+    maps: state.maps
+  }),
+  dispatch => ({
+    switchMap(mapTags) {
+      dispatch(mapActions.switchMap(mapTags))
+    }
+  })
+)(MapView);
--- src/components/NavigationView.js
+++ src/components/NavigationView.js
@@ -17,7 +17,7 @@ const NavigationView = ({navi, switchTab, pushRoute, onNavigateBack}) => {
 
   BackAndroid.addEventListener('hardwareBackPress', () => onNavigateBack())
 
-  const renderScene = (sceneProps) =>{
+  const renderScene = (sceneProps) => {
     return (
       <View style={styles.sceneContainer}>
         {AppRouter(sceneProps)}
@@ -27,7 +27,7 @@ const NavigationView = ({navi, switchTab, pushRoute, onNavigateBack}) => {
 
   return (
     <View style={styles.container}>
-      <NavigationBar 
+      <NavigationBar
         tabs={tabs}
         currentTabIndex={tabs.get('index')}
         switchTab={switchTab}
