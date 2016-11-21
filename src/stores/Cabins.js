d5bf48fe819deeb8b8b819683d719b8628dc5897
Julius Rajala <jijraj@utu.fi>
2016-11-22 00:18:10 +0200

    Started working on integrating asyncstorage into app.

--- src/stores/Cabins.js
+++ src/stores/Cabins.js
@@ -1,6 +1,11 @@
 // This store is for storing cabin data.
 import { fromJS, Map, List } from 'immutable';
 import createAction from '../utils/createAction';
+import Store from 'react-native-store';
+
+const localStorage = {
+  'cabins': Store.model('cabins')
+};
 
 export const cabinActionsTypes = {
   cabin_added: 'cabin_added',
@@ -17,20 +22,27 @@ export const cabinActions = {
   }
 };
 
+function getCabinsFromStorage(){
+  return Promise.resolve(localStorage.cabins.find())
+    .then(resp => resp) 
+}
+
 const initialState = fromJS({
-  homeCabin: '****',
-  cabins: {}
+  cabins: getCabinsFromStorage() || {}
 });
 
 function cabins(state=initialState, action) {
-  console.log(action);
+  console.log(action, state.toJS());
   switch(action.type) {
     case cabinActionsTypes.cabin_added:
-      // With immutable
-      console.log('action payload', action.payload.toJS())
+      const cabin = action.payload;
+      // localStorage.cabins.add({
+      //   cabinNumber: cabin.get('cabinNumber'), 
+      //   cabinDescription: cabin.get('cabinDescription')
+      // });
       return state.setIn(['cabins', action.payload.get('cabinNumber')], action.payload);
     case cabinActionsTypes.cabin_removed:
-      console.log('payload, cabinNumber', action.payload, state.get('cabins').toJS());
+      localStorage.cabins.remove(item => item.cabinNumber !== action.payload);
       return state.set('cabins', state.get('cabins').filter(cabin => cabin.get('cabinNumber') !== action.payload));
     default:
       return state;
